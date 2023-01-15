6de9fb7836aa6db616371ca8e7c7d4e609ff9196
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-14 23:32:21 -0300

    reminder before start 2.2

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+<section>
+      <h1>Portfolio 3d</h1>
+
+      <input type="text" class="inputTipeText" />
+      <button class="addButton">add</button>
+      <button class="deleteButton">delete</button>
+      <button class="deleteAllButton">delete all</button>
+      <h2 class="addText"></h2>
+</section>
+*/
+
+const inputText = document.querySelector(".inputTipeText");
+const addButton = document.querySelector(".addButton");
+
+addButton.addEventListener("click", function () {
+  console.log("clicked");
+});
