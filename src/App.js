01e52c5f88337910cb176e162117b5f82f00f264
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-15 04:34:05 -0300

    assets localStorage darkMode

--- src/App.js
+++ src/App.js
@@ -31,6 +31,185 @@ function App() {
 
   return (
     <>
+      <header
+        className={`header ${dark ? "dark-mode scroll-header-dark" : ""}`}
+      >
+        <nav className="nav container">
+          <div className="left-side">
+            <a href="index.html" className="nav__logo">
+              Marco
+            </a>
+            <div className="button-dark-mode">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="boxicon-mouse"
+              >
+                <path d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z" />
+              </svg>
+
+              <div
+                className="dark-button-day"
+                onClick={() => setDark((dark) => !dark)}
+              >
+                <div className="testing">
+                  <button
+                    className={`${dark ? "button-bw-night" : "button-bw-day"}`}
+                  ></button>
+                </div>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="boxicon-mouse"
+              >
+                <path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z" />
+              </svg>
+            </div>
+          </div>
+
+          <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+            <ul className="nav__list grid">
+              <li className="nav__item">
+                <a
+                  href="#home"
+                  onClick={() => setActiveNav("#home")}
+                  className={
+                    activeNav === "#home"
+                      ? "nav__link active-link"
+                      : "nav__link "
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="nav__icon boxicon-ligth-static"
+                  >
+                    <path d="M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm7 7v-5h4v5h-4zm2-15.586 6 6V15l.001 5H16v-5c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v5H6v-9.586l6-6z" />
+                  </svg>
+                  Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="#about"
+                  onClick={() => setActiveNav("#about")}
+                  className={
+                    activeNav === "#about"
+                      ? "nav__link active-link"
+                      : "nav__link "
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="nav__icon boxicon-ligth-static"
+                  >
+                    <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z" />
+                  </svg>
+                  About
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="#skills"
+                  onClick={() => setActiveNav("#skills")}
+                  className={
+                    activeNav === "#skills"
+                      ? "nav__link active-link"
+                      : "nav__link "
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="nav__icon boxicon-ligth-static"
+                  >
+                    <path d="M19.903 8.586a.997.997 0 0 0-.196-.293l-6-6a.997.997 0 0 0-.293-.196c-.03-.014-.062-.022-.094-.033a.991.991 0 0 0-.259-.051C13.04 2.011 13.021 2 13 2H6c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V9c0-.021-.011-.04-.013-.062a.952.952 0 0 0-.051-.259c-.01-.032-.019-.063-.033-.093zM16.586 8H14V5.414L16.586 8zM6 20V4h6v5a1 1 0 0 0 1 1h5l.002 10H6z" />
+                    <path d="M8 12h8v2H8zm0 4h8v2H8zm0-8h2v2H8z" />
+                  </svg>
+                  Skills
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="#portfolio"
+                  onClick={() => setActiveNav("#portfolio")}
+                  className={
+                    activeNav === "#portfolio"
+                      ? "nav__link active-link"
+                      : "nav__link "
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="nav__icon boxicon-ligth-static"
+                  >
+                    <path d="M2.165 19.551c.186.28.499.449.835.449h15c.4 0 .762-.238.919-.606l3-7A.998.998 0 0 0 21 11h-1V7c0-1.103-.897-2-2-2h-6.1L9.616 3.213A.997.997 0 0 0 9 3H4c-1.103 0-2 .897-2 2v14h.007a1 1 0 0 0 .158.551zM17.341 18H4.517l2.143-5h12.824l-2.143 5zM18 7v4H6c-.4 0-.762.238-.919.606L4 14.129V7h14z" />
+                  </svg>
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="#contact"
+                  onClick={() => setActiveNav("#contact")}
+                  className={
+                    activeNav === "#contact"
+                      ? "nav__link active-link"
+                      : "nav__link "
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="nav__icon boxicon-ligth-static"
+                  >
+                    <path d="m21.426 11.095-17-8A.999.999 0 0 0 3.03 4.242L4.969 12 3.03 19.758a.998.998 0 0 0 1.396 1.147l17-8a1 1 0 0 0 0-1.81zM5.481 18.197l.839-3.357L12 12 6.32 9.16l-.839-3.357L18.651 12l-13.17 6.197z" />
+                  </svg>
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="nav__close boxicon-check cross-navbar"
+              onClick={() => showMenu(!Toggle)}
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z" />
+            </svg>
+          </div>
+          <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="boxicon-check"
+            >
+              <path d="M10 3H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM9 9H5V5h4v4zm5 2h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1zm1-6h4v4h-4V5zM3 20a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v6zm2-5h4v4H5v-4zm8 5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v6zm2-5h4v4h-4v-4z" />
+            </svg>
+          </div>
+        </nav>
+      </header>
       <main className="main">
         {/* <Home mode={dark} /> */}
         <About mode={dark} />
