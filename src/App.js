5c122b016efc272ecdada2bcfe96441ef5532814
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-15 04:20:28 -0300

    assets footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from "./components/about/About";
+import Footer from "./components/footer/Fotter";
 import "./App.css";
 
 function App() {
   return (
     <>
       <About />
+      <Footer />
     </>
   );
 }
