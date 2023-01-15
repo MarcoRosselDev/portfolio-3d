ef04b41323e40eaa85188aead833ae368ee84948
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-14 23:59:02 -0300

    reminder before start 2.5

--- app.js
+++ app.js
@@ -17,11 +17,15 @@ const addText = document.querySelector(".addText");
 let inner = [];
 
 addButton.addEventListener("click", function () {
+  //
   inner.push(inputText.value);
 
-  // addText.innerHTML = "<h2>hola</h2>";
-  addText.innerHTML = `<h2>${inner}</h2>`;
-  console.log(inner);
-  console.log(inner.push);
+  let printDom = "";
+
+  for (let i = 0; i < inner.length; i++) {
+    printDom = `<h2>${inner}</h2>`;
+  }
+
+  addText.innerHTML = `<h2>${printDom}</h2>`;
   inputText.value = "";
 });
