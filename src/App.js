54abdc92c85b50da47d70f7146db9be57e069854
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-15 04:33:03 -0300

    assets localStorage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import About from "./components/about/About";
 import Footer from "./components/footer/Fotter";
 import Skills from "./components/skills/Skills";
@@ -5,12 +6,41 @@ import ScrollUp from "./components/scrollup/ScrollUp";
 import "./App.css";
 
 function App() {
+  /*============= Change Background header ============= */
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
+
+  /*============= Toggle Menu ============= */
+  const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
+
+  /*============= Dark mode ============= */
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const data = window.localStorage.getItem("darkmode1");
+    if (data !== null) setDark(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("darkmode1", JSON.stringify(dark));
+  }, [dark]);
+
   return (
     <>
-      <About />
-      <Skills />
-      <Footer />
-      <ScrollUp />
+      <main className="main">
+        {/* <Home mode={dark} /> */}
+        <About mode={dark} />
+        <Skills mode={dark} />
+        {/* <Portfolio mode={dark} /> */}
+        {/* <Contact mode={dark} /> */}
+      </main>
+
+      <Footer mode={dark} />
+      <ScrollUp mode={dark} />
     </>
   );
 }
