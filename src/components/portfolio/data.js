226dea823af8d959ca4c4b464bf085ce79939951
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-16 04:39:51 -0300

    data projects

---
+++ src/components/portfolio/data.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    id: 1,
+    title: "Fitnes Page",
+    image: "commig soon",
+    descriptions:
+      "Fitnes Page were i try to work whit out the lasted vertion of React Js",
+    libraries: ["React", "css", "lo que sea"],
+    link: "",
+  },
+  {
+    id: 1,
+    title: "3D Room",
+    image: "commig soon",
+    descriptions:
+      "Fitnes Page were i try to work whit out the lasted vertion of React Js",
+    libraries: ["React", "css", "lo que sea"],
+    link: "",
+  },
+  {
+    id: 1,
+    title: "celphon e-comert",
+    image: "commig soon",
+    descriptions:
+      "Fitnes Page were i try to work whit out the lasted vertion of React Js",
+    libraries: ["React", "css", "lo que sea"],
+    link: "",
+  },
+];
