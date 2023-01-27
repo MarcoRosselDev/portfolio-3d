274261efe2badccdab1889cdbe91a4067e2ad708
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-27 01:14:30 -0300

    adding new project

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -19,12 +19,12 @@ export default [
   },
   {
     id: 3,
-    title: "Login Firebase",
+    title: "movie search",
     image: "commig soon",
     descriptions:
       "Fitnes Page were i try to work whit out the lasted vertion of React Js",
     libraries: ["React", "css", "lo que sea"],
-    link: "https://login-firebase-370419.nn.r.appspot.com/login",
+    link: "https://incandescent-druid-e495ca.netlify.app/",
   },
   {
     id: 4,
